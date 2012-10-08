19454becb2c068662b0f7b681a2ac691cc23c59f
Cas Gentry <cas.gentry@yahoo.com>
2012-10-07 22:43:07 -0400

    pull out login menu

---
+++ loginmenu.php
@@ -0,0 +1,28 @@
+<?php
+	if ( @$_SESSION['login'] != "true" ){
+		echo "<div id=\"login\">";
+		echo "<form action=\"index.php?dologin=yes\" method=\"POST\">";
+		echo "<table><tr>";
+		echo "<td class=\"tableTag\">E-mail</td></tr>";
+		echo "<tr><td><input type=\"text\" name=\"username\" class=\"logInput\" maxlength=\"50\"/></td></tr>";
+			
+		echo "<tr><td><p class=\"errorMsg\">";
+		if(@$_SESSION['mismatch']=="true"){
+			echo "Incorrect username or password.";
+		}
+		elseif(@$_SESSION['unregistered']=="true"){
+			echo "E-mail not yet registered.";
+		}
+	
+		echo "</p></tr><tr><td class=\"tableTag\">Password</td></tr>";
+		echo "<tr><td><input type=\"password\" name=\"pwd\" class=\"logInput\" maxlength=\"50\"/>";
+		echo "</td></tr><tr>";
+		echo "<td><div class=\"signup\">Not yet a member?  <a href=\"register.php\">Sign up</a> today.</div></td>";
+		echo "</tr><tr><td><input type=\"submit\" class=\"logButton\" value=\"Login\"/></td></tr>";
+		echo "</table></form>";
+		echo "</div>";
+	}
+	else{
+		include("userMenu.php");
+	}
+?>
